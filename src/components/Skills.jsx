999b311305cba607e71a517d6fc9435e2bc6ca6c
Robert-Thaiyah <bobthaiya@gmail.com>
2023-08-19 16:34:01 +0300

    animations added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,55 +1,103 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
-import HTML from '../assets/html.png';
-import CSS from '../assets/css.png';
-import JavaScript from '../assets/javascript.png';
-import ReactImg from '../assets/react.png';
-import GitHub from '../assets/github.png';
-import Tailwind from '../assets/tailwind.png';
-
-
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import GitHub from "../assets/github.png";
+import Tailwind from "../assets/tailwind.png";
 
 export const Skills = () => {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
-                <p className='py-4'> // These are the technologies I've worked with</p>
-            </div>
-
-            <div className='w-full grid grid-cols-2 sm:grid-cols-3 text-center py-8 gap-10'>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img src={HTML} alt="HTML Icon" className='w-20 mx-auto'/>
-                    <p className='py-4'>HTML</p>
-                </div>
-
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img src={CSS} alt="CSS Icon" className='w-20 mx-auto'/>
-                    <p className='py-4'>CSS</p>
-                </div>
-
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img src={JavaScript} alt="Javascript Icon" className='w-20 mx-auto'/>
-                    <p className='py-4'>Javascript</p>
-                </div>
-
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img src={ReactImg} alt="React Icon" className='w-20 mx-auto'/>
-                    <p className='py-4'>React</p>
-                </div>
-
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img src={GitHub} alt="Github Icon" className='w-20 mx-auto'/>
-                    <p className='py-4'>Github</p>
-                </div>
-
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img src={Tailwind} alt="Tailwind Icon" className='w-20 mx-auto'/>
-                    <p className='py-4'>Tailwind</p>
-                </div>
-            </div>
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+            Skills
+          </p>
+          <p className="py-4">// These are the technologies I've worked with</p>
         </div>
+
+        <motion.div
+          className="w-full grid grid-cols-2 sm:grid-cols-3 text-center py-8 gap-10"
+          variants={container}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+        >
+          <motion.div
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+            variants={item}
+          >
+            <img src={HTML} alt="HTML Icon" className="w-20 mx-auto" />
+            <p className="py-4">HTML</p>
+          </motion.div>
+
+          <motion.div
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+            variants={item}
+          >
+            <img src={CSS} alt="CSS Icon" className="w-20 mx-auto" />
+            <p className="py-4">CSS</p>
+          </motion.div>
+
+          <motion.div
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+            variants={item}
+          >
+            <img
+              src={JavaScript}
+              alt="Javascript Icon"
+              className="w-20 mx-auto"
+            />
+            <p className="py-4">Javascript</p>
+          </motion.div>
+
+          <motion.div
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+            variants={item}
+          >
+            <img src={ReactImg} alt="React Icon" className="w-20 mx-auto" />
+            <p className="py-4">React</p>
+          </motion.div>
+
+          <motion.div
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+            variants={item}
+          >
+            <img src={GitHub} alt="Github Icon" className="w-20 mx-auto" />
+            <p className="py-4">Github</p>
+          </motion.div>
+
+          <motion.div
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+            variants={item}
+          >
+            <img src={Tailwind} alt="Tailwind Icon" className="w-20 mx-auto" />
+            <p className="py-4">Tailwind</p>
+          </motion.div>
+        </motion.div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/data/data.js
+++ src/data/data.js
@@ -1,56 +1,52 @@
+import Summer from "../assets/Skillsense.PNG";
+import Sweets from "../assets/Sweets.PNG";
+import Stellar from "../assets/Stellar.PNG";
+import Interio from "../assets/Interio.PNG";
+import Gentium from "../assets/Gentium.PNG";
+import Denti from "../assets/Denti.PNG";
 
-import SkillSense from '../assets/Skillsense.PNG'
-import Sweets from '../assets/Sweets.PNG'
-import Stellar from '../assets/Stellar.PNG'
-import Interio from '../assets/Interio.PNG'
-import Gentium from '../assets/Gentium.PNG'
-import Denti from '../assets/Denti.PNG'
+export const data = [
+  {
+    id: 1,
+    name: "Interio Designers Landing Page",
+    image: Interio,
+    github: "https://github.com/Robert-Thaiyah/interio-landing",
+    live: "https://interio-landing.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Gentium Agency Landing Page",
+    image: Gentium,
+    github: "https://github.com/Robert-Thaiyah/gentium-landing",
+    live: "https://gentium-agency.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Denti Dental Landing Page",
+    image: Denti,
+    github: "https://github.com/Robert-Thaiyah/denti-dental",
+    live: "https://dental-denti.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "Sweet Delights Payment Receipt",
+    image: Sweets,
+    github: "",
+    live: "https://sweet-delights-receipt.netlify.app/",
+  },
 
-
-export const data=[
-    {
-        id:1,
-        name:"Interio Designers Landing Page",
-        image:Interio,
-        github:"https://github.com/Robert-Thaiyah/interio-landing",
-        live:"https://interio-landing.netlify.app/",
-    },
-    {
-        id:2,
-        name:"Gentium Agency Landing Page",
-        image:Gentium,
-        github:"https://github.com/Robert-Thaiyah/gentium-landing",
-        live:"https://gentium-agency.netlify.app/",
-    },
-    {
-        id:3,
-        name:"Denti Dental Landing Page",
-        image:Denti,
-        github:"https://github.com/Robert-Thaiyah/denti-dental",
-        live:"https://dental-denti.netlify.app/",
-    },
-    {
-        id:4,
-        name:"Sweet Delights Newsletter",
-        image:Sweets,
-        github:"",
-        live:"https://sweet-delights.netlify.app/",
-    },
-
-    {
-        id:5,
-        name:"Stellar Homes Newsletter",
-        image:Stellar,
-        github:"",
-        live:"https://stellar-reality.netlify.app/",
-    },
-    {
-        id:6,
-        name:"SkillSense Newsletter",
-        image:SkillSense,
-        github:"",
-        live:"https://skillsense.netlify.app/",
-    },
-
-
-]
+  {
+    id: 5,
+    name: "Stellar Homes Newsletter",
+    image: Stellar,
+    github: "",
+    live: "https://stellar-realty.netlify.app/",
+  },
+  {
+    id: 6,
+    name: "Fred & Sylvia Boutique Promotion",
+    image: Summer,
+    github: "",
+    live: "https://fred-and-sylvia-boutique.netlify.app/",
+  },
+];
